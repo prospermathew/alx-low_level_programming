c92bcd2b602e4f5e384a42f0cbaa73ff6f6930d4
prospermathew <prospermathew.gontolono@gmail.com>
2022-12-20 15:32:49 -0800

    Hot Fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,21 @@
-   include "main.h"
 
-  /** 
-   *puts2 - prints string, followed by a new line, 
-   * @str: pointer to the string to point 
-   * Return: void
-   */
-   int void_puts2(char *str) 
-  {
-      int i = 0;
+/**
+ * puts2 -  prints a string
+ *
+ * @str: arg *str
+ *
+ * Return: string to stdout
+ */
 
-     while (str[i] != '\0' ) 
-{   
-     if (i % 2 == 0) 
-   {
-        _putchar(str[i]);
-} 
-     i++;
-  } 
-     _putchar('\n');
-} 
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
