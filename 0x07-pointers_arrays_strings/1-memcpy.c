2d107cfca3ac58e0839f1ccbe603cd16472b3842
prospermathew <prospermathew.gontolono@gmail.com>
2023-01-03 11:12:40 -0800

    Mandatory Tasks

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,25 @@
 #include "main.h"
 /**
- * *_memcpy - copy memory area
- *
- * @dest: char
- * @src: char source
- * @n: integer
- *
- * Return: char
+ *_memcpy -   copy number bytes form adress "from" to adress "to"
+ *@dest: content is to be copied
+ *@src: Source of data to be copied
+ *@n: bytes of the memory
+ *Return: dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+	while (i < n)  /*Delacring WHILE*/
 	{
-		dest[i] = src[i];
-	}
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
+		i++;
+
+	} /*END WHILE*/
+
 
 	return (dest);
 }
