f94bd32d06d31c04307bd0fdb6366f1edaf5d9f5
prospermathew <prospermathew.gontolono@gmail.com>
2023-01-05 13:04:29 -0800

    Advanced Tasks

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,6 @@
+ #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
-#include <stdio.h>                                /**
- * void prototypes(void)
- * int prototypes(int)                             * char prototypes(char)
- */
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -12,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
