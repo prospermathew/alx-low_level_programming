70fbb5a32e5d5f4562d70aba1a68b664ed7569cd
prospermathew <prospermathew.gontolono@gmail.com>
2023-01-09 18:48:39 -0800

    first commit

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,82 @@
+0x09 Static Libraries
+0-isupper.c
+
+                
+
+#include "main.h"
+
+/**
+
+ * _isupper - uppercase letters
+
+ * @c: char to check
+
+ *
+
+ * Return: 0 or 1
+
+ */
+
+int _isupper(int c)
+
+{
+
+        if (c >= 'A' && c <= 'Z')
+
+                return (1);
+
+        else
+
+        return (0);
+
+}
+
+ 
+
+ 
+
+
+0-memset.c
+
+                
+
+#include "main.h"
+
+/**
+
+ * _memset - fill a block of memory with a specific value
+
+ * @s: starting address of memory to be filled
+
+ * @b: the desired value
+
+ * @n: number of bytes to be changed
+
+ *
+
+ * Return: changed array with new value for n bytes
+
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+        int i = 0;
+
+
+        for (; n > 0; i++)
+
+        {
+
+                s[i] = b;
+
+                n--;
+
+        }
+
+        return (s);
+
+}
+
+ 
